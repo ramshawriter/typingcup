ba2ac80efe84d8925f9d3d4445ad963b892c7f66
ramshawriter <ramshawriter5@gmail.com>
2026-06-28 22:42:52 +0500

    Discourage search engine indexing (noindex + robots disallow)

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,10 +1,9 @@
 import { SITE } from '../consts.js';
 
 export async function GET() {
+  // Discourage all search engines from indexing the site.
   const body = `User-agent: *
-Allow: /
-
-Sitemap: ${new URL('sitemap-index.xml', SITE.url).href}
+Disallow: /
 `;
   return new Response(body, {
     headers: { 'Content-Type': 'text/plain; charset=utf-8' },
